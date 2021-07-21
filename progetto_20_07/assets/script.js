4af128b3fe90dedd313c0613bd8841d4bcff0e85
Federico Scarlata <fedesscarlata@gmail.com>
2021-07-21 09:47:12 +0200

    aggiunta card

--- progetto_20_07/assets/script.js
+++ progetto_20_07/assets/script.js
@@ -14,9 +14,21 @@ $.ajax({
             '<h2 class="accordion-header" id="headingTwo">'+
               '<button id="nomeUtente" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+dati.data[i].first_name+'" aria-expanded="false" aria-controls="collapseTwo">' + dati.data[i].first_name+ ' ' + dati.data[i].last_name +'</button> </h2>'
               + '<div id="collapse'+dati.data[i].first_name+'" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">'+
-            '  <div class="accordion-body" id="infoCard">'+
-            '<img class="mx-auto" src="'+dati.data[i].avatar+'">'+
-            '<p>' + dati.data[i].email + '</p>' +
+            '<div class="accordion-body" id="infoCard">'+
+
+            // card
+            '<div class="card" style="width: 18rem;">'+
+            '<img class="card-img-top" src="'+dati.data[i].avatar+'" alt="Card image cap">'+
+            '<div class="card-body">'+
+              '<h5 class="card-title">'+dati.data[i].first_name+'-'+dati.data[i].last_name +'</h5>'+
+              '<p class="card-text">'+ dati.data[i].email +'</p>'+
+              '<a href="#" class="card-link">Modifica</a>'+
+              '<a href="#" class="card-link">Elimina</a>'+
+            '</div>'+
+          '</div>'+
+
+            // '<img class="mx-auto" src="'+dati.data[i].avatar+'">'+
+            // '<p>' + dati.data[i].email + '</p>' +
 
              '</div></div>')
         }
